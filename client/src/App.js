47fa38f68dc744035ceb73a73dd1cb1d1fbd7999
Derimar Gray <derimargray@gmail.com>
2021-10-25 01:31:09 -0500

    add final mutations setting and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,32 @@
-import React from "react";
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-} from "@apollo/client";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import NoMatch from "./pages/NoMatch";
-import SingleThought from "./pages/SingleThought";
-import Profile from "./pages/Profile";
-import Signup from "./pages/Signup";
-import { setContext } from '@apollo/client/link/context';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
-  const httpLink = createHttpLink({
-    uri: "/graphql",
-  });
-
-  const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('id_token');
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-
   return (
     <ApolloProvider client={client}>
       <Router>
@@ -48,6 +39,7 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/profile/:username?" component={Profile} />
               <Route exact path="/thought/:id" component={SingleThought} />
+
               <Route component={NoMatch} />
             </Switch>
           </div>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,26 +1,30 @@
-import React from "react";
-import { Redirect, useParams } from "react-router-dom";
-import ThoughtList from "../components/ThoughtList";
-import { useQuery, useMutation } from "@apollo/client";
-import { QUERY_USER, QUERY_ME } from "../utils/queries";
-import FriendList from "../components/FriendList";
-import Auth from "../utils/auth";
-import { ADD_FRIEND } from "../utils/mutations";
-import ThoughtForm from '../components/ThoughtForm';
+import React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
 
-const Profile = () => {
+import ThoughtForm from '../components/ThoughtForm';
+import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
+
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Profile = props => {
   const { username: userParam } = useParams();
 
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
+    variables: { username: userParam }
   });
 
   const user = data?.me || data?.user || {};
 
-  const [addFriend] = useMutation(ADD_FRIEND);
-
-  // redirect to personal profile page if username is the logged-in user's
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+  // redirect to personal profile page if username is yours
+  if (
+    Auth.loggedIn() &&
+    Auth.getProfile().data.username === userParam
+  ) {
     return <Redirect to="/profile" />;
   }
 
@@ -31,8 +35,7 @@ const Profile = () => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links
-        above to sign up or log in!
+        You need to be logged in to see this. Use the navigation links above to sign up or log in!
       </h4>
     );
   }
@@ -40,7 +43,7 @@ const Profile = () => {
   const handleClick = async () => {
     try {
       await addFriend({
-        variables: { id: user._id },
+        variables: { id: user._id }
       });
     } catch (e) {
       console.error(e);
@@ -51,7 +54,7 @@ const Profile = () => {
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : "your"} profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
 
         {userParam && (
@@ -63,10 +66,7 @@ const Profile = () => {
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
-          <ThoughtList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-          />
+          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
         </div>
 
         <div className="col-12 col-lg-3 mb-3">
